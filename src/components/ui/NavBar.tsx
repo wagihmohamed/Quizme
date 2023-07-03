a0e5dedf134381a322c1154e8c240cc79e965188
wagihmohamed <wagihm969@gmail.com>
2023-07-04 01:17:20 +0300

    fixed mobile layout in home

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -6,7 +6,7 @@ import { ModeToggle } from "./ModeToggle";
 export const NavBar = () => {
   return (
     <header className="mx-auto max-w-7xl p-8 pt-4 border-b-2 pb-3">
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 justify-center items-center md:justify-between md:items-center px-5">
+      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 justify-center items-center sm:justify-between sm:items-center px-5">
         <Link
           className="text-2xl font-bold text-gray-900 dark:text-gray-100"
           href="/"
