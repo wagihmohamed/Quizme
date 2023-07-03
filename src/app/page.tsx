2c2f27574d168661e185c492658a6dc7f5e032d1
wagihmohamed <wagihm969@gmail.com>
2023-07-04 01:16:03 +0300

    fixed mobile layout in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
       <main className="max-w-7xl mx-auto text-center">
         <div className="mt-16">
           <p className="text-primary/70 text-2xl">Explore QUIZME</p>
-          <h1 className="text-primary text-7xl font-semibold mt-3">
+          <h1 className="text-primary text-6xl font-semibold mt-3">
             Test your knowledge <br /> with our quizes
           </h1>
         </div>
         <Image
           src={LandingImage}
           alt="Landing Image"
-          className="mx-auto min-w-full h-[500px]"
+          className="mx-auto w-full h-[30rem]"
         />
         <div className="flex justify-center gap-10 items-center mt-10">
           <Link className={buttonVariants({
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -5,8 +5,8 @@ import { ModeToggle } from "./ModeToggle";
 
 export const NavBar = () => {
   return (
-    <header className="mx-auto max-w-7xl pt-4 border-b-2 pb-3">
-      <div className="flex justify-between items-center px-5">
+    <header className="mx-auto max-w-7xl p-8 pt-4 border-b-2 pb-3">
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 justify-center items-center md:justify-between md:items-center px-5">
         <Link
           className="text-2xl font-bold text-gray-900 dark:text-gray-100"
           href="/"
